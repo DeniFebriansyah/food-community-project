3923686b11a42ef36184675fb02b0e479c19206d
DeniFebriansyah <denifebriansyah23@gmail.com>
2023-03-20 21:21:33 +0700

    First Commit

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <p>Deni's Foodary © 2023</p>
+    </div>
+  );
+};
+export default Footer;
---
+++ src/recipe.js
@@ -0,0 +1,32 @@
+export const my_recipe = {
+  recipe_1: {
+    name: "Chirashi Don",
+    image: "../img/recipe_1.jpg",
+    preparation_time: "30 minutes",
+    servings: "2",
+    ingredients: [
+      "1/2 cup rice",
+      "1/2 cup water",
+      "2 eggs",
+      "300g of salmon, tuna and salmon roe",
+    ],
+    instructions: [
+      "Cook rice and water in a rice cooker",
+      "Fry eggs in a pan",
+      "Serve rice, eggs and fish on a plate",
+    ],
+  },
+  recipe_2: {
+    name: "Omelet",
+    image: "../img/recipe_2.jpg",
+    preparation_time: "2 minutes",
+    servings: "1",
+    ingredients: ["2 eggs", "30 ml water", "Pinch salt", "Pinch pepper"],
+    instructions: [
+      "Whisk eggs, water, salt and pepper",
+      "Spray 8-inch (20 cm) non-stick skillet with cooking spray. Heat over medium heat. Pour in egg mixture. As eggs set around edge of skillet, with spatula, gently push cooked portions toward centre of skillet. Tilt and rotate skillet to allow uncooked egg to flow into empty spaces.",
+      "When eggs are almost set on surface but still look moist, cover half of omelette with filling. Slip spatula under unfilled side; fold over onto filled half.",
+      "Cook for a minute, then slide omelette onto plate",
+    ],
+  },
+};
